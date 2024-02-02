266281990dc79852cc68f2a56a9b61510147a80f
he-xiaotaoEC <243978580@qq.com>
2024-02-02 16:35:27 +0800

    premake support and gitignore

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	defines "YAML_CPP_STATIC_DEFINE"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
